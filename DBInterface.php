1667eae9d625eb5662beef033bbab4237c3cb077
George Wilson <g.wilson@lancsater.ac.uk>
2016-12-02 19:40:07 +0000

    finished DB Interface

--- DBInterface.php
+++ DBInterface.php
@@ -3,21 +3,42 @@
 /**
  *
  */
-class DBInterface extends AnotherClass
+class DBInterface
 {
-  $COMMANDS = array();
+  public $COMMANDS = array();
   function __construct(){
-    $COMMANDS = json_decode(file_get_contents("./DBCommands.json"));
+    $this->COMMANDS = json_decode(file_get_contents("./DBCommands.json"),true);
   }
   function getDBCommands(){
-    return $COMMANDS
+    return $this->COMMANDS;
   }
-  function runDBCOMMAND($COMMAND, $ArrayOfParams){
+  function runDBCOMMAND($COMMAND, $ArrayOfParams = Array()){
     //format of ArrayOfParams unkknown
     var_dump($this->COMMANDS);
+    if($Command = $this->getCommand($COMMAND,$ArrayOfParams)){
+      var_dump($Command);
+    }
+    else{
+      die("Unknown DB COMMAND");
+    }
+  }
+  function getCommand($COMMAND,$ArrayOfParams){
+    foreach ($this->COMMANDS as $value) {
+      if($value["name"] == $COMMAND){
+        $sql = $value["SQL"];
+        foreach ($value["params"] as $param) {
+          if(strpos($ArrayOfParams[$param],";")!== false){
+            return false;
+          }
+          $sql = str_replace($param,$ArrayOfParams[$param],$sql);
+        }
+        return $sql;
+      }
+    }
+    return false;
   }
 }
 $DB = new DBInterface();
-$DB->runDBCOMMAND();
+$DB->runDBCOMMAND("getPollData",Array("POLLID"=>"23895"));
 
  ?>
