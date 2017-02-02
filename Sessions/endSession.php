306d118ec3685646db03fa845de680800184e1f2
George Wilson <george.wilson.2905@gmail.com>
2017-02-02 21:49:12 +0000

    testing endSession

--- Sessions/endSession.php
+++ Sessions/endSession.php
@@ -6,8 +6,13 @@ class EndSession extends API
     $this->POSTVarsReq = array('SESSIONID');
   }
   public function doAPI(){
-    
-    $res = $this->DB->runDBCOMMAND("endSession",array('SESSIONID'=>$_POST['SESSIONID'], 'LENGTH'=> ));
+    $SessionRes = $this->DB->runDBCOMMAND("getSession",array("SESSIONID"=>$_POST['SESSIONID']));
+    if(1 != mysqli_num_rows($SessionRes)){
+      $GLOBALS['dieSafely'](true,"Session not valid");
+    }
+    $Session = mysqli_fetch_assoc($SessionRes);
+    $length = (time() - $Session['StartTime'])/60;
+    $res = $this->DB->runDBCOMMAND("endSession",array('SESSIONID'=>$_POST['SESSIONID'], 'LENGTH'=> $length));
     if($res !== FALSE  && mysqli_num_rows($res) == 0)
       return (true);
     return false;
