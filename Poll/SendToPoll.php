68a01c2bb70d8b17609a95a3730e844befc23115
George Wilson <george.wilson.2905@gmail.com>
2017-03-13 21:33:29 +0000

    MultiPoll

--- Poll/SendToPoll.php
+++ Poll/SendToPoll.php
@@ -4,14 +4,14 @@ include_once '../API.php';
 class SendToPoll extends API
 {
   function preInit(){
-    $this->GETVarsReq = array('VOTE','ID');
+    $this->GETVarsReq = array('VOTE','SESSIONID');
   }
   public function doAPI(){
     $VOTE = strtoupper($_GET['VOTE']);
     if($VOTE!="A" && $VOTE!="B" && $VOTE!="C" && $VOTE != "D")
       $GLOBALS['dieSafely'](true,"Invalid Vote");
 
-    $res = $this->DB->runDBCOMMAND("addToPoll",array('POLLID'=> $_GET['ID'] ,'VOTE' => $VOTE));
+    $res = $this->DB->runDBCOMMAND("addToPoll",array('POLLID'=> $_GET['SESSIONID'] ,'VOTE' => $VOTE));
     if($res !== FALSE)
       return "Submitted";
     $GLOBALS['dieSafely'](true,"SQL Fail");
